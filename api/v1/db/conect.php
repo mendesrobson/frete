ec89625a5f9cc9d2d4817499a97ccfa959861338
Robson Mendes <robsonmendes_1@outlook.com>
2021-03-25 21:49:41 -0300

    removendo a conexao

--- api/v1/db/conect.php
+++ api/v1/db/conect.php
@@ -1,9 +1,9 @@
 <?php 
     error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-    $serv = 'br140.hostgator.com.br';
-    $name = 'catal842_mortorf';
-    $password = 'mortorf123';
-    $banco = 'catal842_MotorFort';
+    $serv = '';
+    $name = '';
+    $password = '';
+    $banco = '';
     $conn = mysql_connect($serv,$name,$password) or die(mysql_error());
     mysql_select_db($banco,$conn) or die(mysql_error());
 ?>
